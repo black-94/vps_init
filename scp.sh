70a432bc733b4ec38d05e43a80c888d79641247a
shangzheng <shangzheng@pinduoduo.com>
2020-09-29 18:24:19 +0800

    commit

--- scp.sh
+++ scp.sh
@@ -4,25 +4,25 @@ set timeout 60
 set cmd [lindex $argv 0]
 set password [lindex $argv 1]
 
-echo "$cmd"
+puts "$cmd"
 spawn $cmd
 expect {
   "*assword*" {
-    echo "send password"
+    puts "send password"
     send "$password\r"
     exp_continue
   }
   "*yes/no*" {
-    echo "send yes"
+    puts "send yes"
     send "yes\r"
     exp_continue
   }
   "*100%*" {
-    echo "cmd end"
+    puts "cmd end"
     exit 0
   }
   eof {
-    echo "cmd end"
+    puts "cmd end"
     exit 0
   }
 }
