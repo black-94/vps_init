e56fb43ccdb779c94cbf20664c7a215d4277dba9
shangzheng <shangzheng@pinduoduo.com>
2020-10-01 08:31:31 +0800

    commit

--- loop.py
+++ loop.py
@@ -122,9 +122,11 @@ def v2rayCheck():
 
 def downloadV2ray():
     if role == 'end':
-        os.system("wget -O /root/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v4.27.5/v2ray-linux-64.zip")
+        os.system(
+            "wget -O /root/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v4.27.5/v2ray-linux-64.zip")
     else:
-        os.system("expect ./scp.ex "+"scp"+" 'root@"+root_ip+":/root/v2ray/v2ray.zip"+" /root/v2ray/' "+" '"+root_passwd+"' >> /tmp/loop.log")
+        os.system(
+            "expect ./scp.ex " + "scp" + " 'root@" + root_ip + ":/root/v2ray/v2ray.zip" + " /root/v2ray/' " + " '" + root_passwd + "' >> /tmp/loop.log")
 
 
 def log(pre, msg):
@@ -135,7 +137,8 @@ def log(pre, msg):
     mode = "a" if exist else "w"
     file = open("/tmp/loop.log", mode, encoding="utf-8")
     file.write(pre + " : " + msg)
-    print(pre + " : " + msg)
+    t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(t + " , " + pre + " : " + msg + "\n")
     file.close()
 
 
